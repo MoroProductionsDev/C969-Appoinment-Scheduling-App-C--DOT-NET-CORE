604ec1d68a75acb9dd35b583664d35e410c077b9
Raul Rivero Rubio <rriverorubio.education@gmail.com>
2023-09-30 17:27:23 -0700

    Updated the [References] dependicies and the [packages.config] file for the current project using the Nuget package manager. Added the connection string raw text after testing it the connection int the [Program.cs]. The connection code was removed.
    The raw string example is not applicable for a production scenario since it is unencrypted. Research and tried to implement it but it will not be useful for this acedamic work

--- Scheduling Console App/Controller/Program.cs
+++ Scheduling Console App/Controller/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using MySql.Data.MySqlClient;
+using static Scheduling_Console_App.Program;
 
 namespace Scheduling_Console_App
 {
     internal class Program
     {
+        const string dbConnStrngKeyNm = "MySqlDBConn";
+
         static void Main(string[] args)
         {
+
+            string connectionString = ConfigurationManager.ConnectionStrings[dbConnStrngKeyNm].ConnectionString;
+
+            Console.WriteLine(connectionString);
         }
     }
 }
